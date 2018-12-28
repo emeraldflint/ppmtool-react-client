3a6972bcc67c79d2c4d94e772a35e9122abae405
Eugene Kasap <ekasap@basketsavings.com>
2018-12-28 18:01:28 +0200

    Added style validation errors with classnames.

--- src/components/Project/AddProject.js
+++ src/components/Project/AddProject.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import PropTypes from "prop-types"
 import {connect} from "react-redux"
 import {createProject} from "../../action/projectAction";
+import classnames from "classnames";
 
 class AddProject extends Component {
     constructor() {
@@ -69,28 +70,42 @@ class AddProject extends Component {
                                     <div className="form-group">
                                         <input
                                             type="text"
-                                            className="form-control form-control-lg "
+                                            className={classnames("form-control form-control-lg", {
+                                                "is-invalid": errors.projectName
+                                            })}
                                             placeholder="Project Name"
                                             name="projectName"
                                             value={this.state.projectName}
                                             onChange={this.onChange}
                                         />
-                                        <p>{errors.projectName}</p>
+                                        {errors.projectName && (
+                                            <div className="invalid-feedback">
+                                                {errors.projectName}
+                                            </div>
+                                        )}
                                     </div>
                                     <div className="form-group">
                                         <input
                                             type="text"
-                                            className="form-control form-control-lg"
+                                            className={classnames("form-control form-control-lg", {
+                                                "is-invalid": errors.projectIdentifier
+                                            })}
                                             placeholder="Unique Project ID"
                                             name="projectIdentifier"
                                             value={this.state.projectIdentifier}
                                             onChange={this.onChange}
                                         />
-                                        <p>{errors.projectIdentifier}</p>
+                                        {errors.projectIdentifier && (
+                                            <div className="invalid-feedback">
+                                                {errors.projectIdentifier}
+                                            </div>
+                                        )}
                                     </div>
                                     <div className="form-group">
                     <textarea
-                        className="form-control form-control-lg"
+                        className={classnames("form-control form-control-lg", {
+                            "is-invalid": errors.description
+                        })}
                         placeholder="Project Description"
                         name="description"
                         value={this.state.description}
@@ -106,7 +121,11 @@ class AddProject extends Component {
                                             value={this.state.start_date}
                                             onChange={this.onChange}
                                         />
-                                        <p>{errors.description}</p>
+                                        {errors.description && (
+                                            <div className="invalid-feedback">
+                                                {errors.description}
+                                            </div>
+                                        )}
                                     </div>
                                     <h6>Estimated End Date</h6>
                                     <div className="form-group">
