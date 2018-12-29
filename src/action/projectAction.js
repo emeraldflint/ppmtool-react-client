6cddac15dd9b5c0ecaf26d4ab7121ad04b4459c3
Eugene Kasap <ekasap@basketsavings.com>
2018-12-30 01:40:07 +0200

    Added error handlers for UpdateProject component.

--- src/action/projectAction.js
+++ src/action/projectAction.js
@@ -14,9 +14,7 @@ export const createProject = (project, history) => async dispatch => {
 };
 
 export const getProjects = () => async dispatch => {
-
     const res = await axios.get("http://localhost:8080/api/project/all");
-
     dispatch({
         type: GET_PROJECTS,
         payload: res.data
@@ -24,11 +22,13 @@ export const getProjects = () => async dispatch => {
 };
 
 export const getProject = (id, history) => async dispatch => {
-
-    const res = await axios.get(`http://localhost:8080/api/project/${id}`);
-
-    dispatch({
-        type: GET_PROJECT,
-        payload: res.data
-    });
+    try {
+        const res = await axios.get(`http://localhost:8080/api/project/${id}`);
+        dispatch({
+            type: GET_PROJECT,
+            payload: res.data
+        });
+    } catch (error) {
+        history.push("/dashboard");
+    }
 };
